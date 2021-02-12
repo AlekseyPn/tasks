67aa1f2e386d14c782e936d566f0a70f4012350b
fargustian <aleksfers@icloud.com>
2021-02-12 21:51:23 +0500

    Prepare router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    name: "login",
+    path: "/",
+    component: () => import(/* webpackChunkName="Login" */ "@/views/Login"),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    name: "task-board",
+    path: "/task-board",
+    component: () => import(/* webpackChunkName="Login" */ "@/views/Tasks"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach();
+
+export default router;
